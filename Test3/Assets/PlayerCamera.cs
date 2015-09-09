faf917d6e32eb95374c82c6a364d40d3c8c4a771
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-09-09 15:18:45 -0400

    Camera follow

---
+++ Test3/Assets/PlayerCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public float LockedY = 0;
+	public float LockedZ = 0;
+	
+	
+	public GameObject player;
+	
+	
+	void Update()
+	{
+		transform.position = new Vector3(player.transform.position.x, LockedY, LockedZ);
+	}
+}
