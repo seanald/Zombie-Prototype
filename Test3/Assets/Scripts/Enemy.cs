cf308afa5f46322f1a452ddc9b66c8dda991de58
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-09-11 12:47:18 -0400

    Dynamic shadows

---
+++ Test3/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public int moveSpeed;
+
+	private Transform player;
+
+	void Update()
+	{
+		player = GameObject.Find("Player").transform;
+	}
+
+	void FixedUpdate()
+	{
+		if (player)
+		{
+			if (Vector3.Distance(player.transform.position, transform.position) < 10)
+			{
+				float step = moveSpeed * Time.deltaTime;
+				//move towards the player
+				this.transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+			}
+		}
+	}
+}
