bc396d7d694f62117dbad8ce9522f85350278552
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-09-13 19:51:09 -0400

    health and plasma

---
+++ Test3/Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int maxHealth;
+
+	private int curHealth;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.CompareTag("Enemy"))
+		{
+			curHealth--;
+		}
+	}
+}
---
+++ Test3/Assets/Scripts/Plasma.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plasma : MonoBehaviour {
+
+	public int maxPlasma;
+
+	private int curPlasma;
+	// Update is called once per frame
+	void Update () 
+	{
+		if(curPlasma < maxPlasma)
+		{
+			curPlasma++;
+		}
+	}
+}
