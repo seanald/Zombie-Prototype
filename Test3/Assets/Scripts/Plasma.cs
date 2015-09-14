bb3dafa77e038e931c61aa8369a761b0f02a0ab5
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-09-13 20:07:46 -0400

    Plasma script

--- Test3/Assets/Scripts/Plasma.cs
+++ Test3/Assets/Scripts/Plasma.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Plasma : MonoBehaviour {
 
+	public Slider plasmaSlider;
 	public int maxPlasma;
 
 	private int curPlasma;
+
+	void Start()
+	{
+		this.plasmaSlider.maxValue = this.maxPlasma;
+		this.curPlasma = this.maxPlasma;
+	}
 	// Update is called once per frame
 	void Update () 
 	{
+		this.plasmaSlider.value = this.curPlasma;
 		if(curPlasma < maxPlasma)
 		{
 			curPlasma++;
