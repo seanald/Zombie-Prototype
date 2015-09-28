a5225bdb92c6733ae9a849aa9d6c530d2c135ca7
wtshuman <wtshuman@gmail.com>
2015-09-28 16:49:31 -0400

    Prelim-Ghost Stun with projectile

---
+++ Test3/Assets/Scripts/GhostBulletScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostBulletScript : MonoBehaviour {
+
+	float Speed = .4f;
+	float BulletLifeTime = 10f;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.position += Speed * this.gameObject.transform.right;
+
+		if (Time.time - startTime >= BulletLifeTime)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		Destroy(this.gameObject);
+	}
+}
