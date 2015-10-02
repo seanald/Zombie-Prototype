90260a39797fc93944fb67f981a96dcaadd938b1
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-10-02 11:33:19 -0400

    More new resources

--- Test3/Assets/Scripts/Player/Ghost/Animation/GhostAnimatorController.cs
+++ Test3/Assets/Scripts/Player/Ghost/Animation/GhostAnimatorController.cs
@@ -10,10 +10,20 @@ public class GhostAnimatorController : MonoBehaviour
 	{
 		this.controller = this.GetComponentInParent<CharacterController>();
 		this.ghostAnimator = this.GetComponentInChildren<Animator>();
+		this.ghostAnimator.SetBool("right", true);
 	}
 
 	void Update()
 	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			this.ghostAnimator.SetBool("right", true);
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			this.ghostAnimator.SetBool("right", false);
+		}
+
 		if (this.controller.isGrounded && this.controller.velocity != Vector3.zero)
 		{
 			this.ghostAnimator.SetBool("walking", true);
