b1b7080d76810688d5f32862b92b50a4c11e734e
wtshuman <wtshuman@gmail.com>
2015-10-05 02:32:27 -0400

    Werewolf anims and basic logic in needs work

---
+++ Test3/Assets/Scripts/Enemy/BaseballWolf/BaseballWolfMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseballWolfMovement : MonoBehaviour {
+
+	private Animator enemyAnimator;
+	private Transform player;
+	private float distance;
+	private float maxdistance = 50f;
+	
+	void Start()
+	{
+		this.enemyAnimator = this.gameObject.GetComponentInChildren<Animator>();
+		this.player = GameObject.Find("ZombieController").transform;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Vector3.Distance(this.player.position, this.transform.position) > 50 && Vector3.Distance(this.player.position, this.transform.position) < 1000)
+		{
+			float step = 100 * Time.deltaTime;
+			//move towards the player
+			this.transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+
+
+
+				
+			this.enemyAnimator.Play("Batwolf_Walk");
+
+		}
+		else
+		{
+			if(Vector3.Distance(this.player.position, this.transform.position) < 70){
+				RaycastHit hit;
+				Debug.DrawLine(transform.position, transform.right * 100, Color.green);
+				if (Physics.Raycast(transform.position, transform.right, out hit))
+				{
+					distance = hit.distance;
+					//print(distance);
+					//print(hit.transform.tag);
+					if (distance < maxdistance && hit.transform.tag == "Player")
+					{
+						GameObject enemyhit = hit.transform.gameObject;
+						enemyhit.GetComponent<ZombieModel>().Health--;
+					}
+				}
+				this.enemyAnimator.Play("Batwolf_Swing");
+			}
+			else{
+				this.enemyAnimator.Play("Batwolf_Stand");
+			}
+		}
+	}
+}
--- Test3/Assets/Scripts/Player/Zombie/Attacks/ZombieAttack.cs
+++ Test3/Assets/Scripts/Player/Zombie/Attacks/ZombieAttack.cs
@@ -5,14 +5,14 @@ public class ZombieAttack : MonoBehaviour
 {
 	
 	private float distance;
-	private float maxdistance = 1.2f;
+	private float maxdistance = 50f;
 	public Vector3 offset;
 	
 	// Use this for initialization
 	void FixedUpdate()
 	{
 
-		if (Input.GetKey(KeyCode.X))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			//Test to see if enemy is in range, if so take it takes damage while kicking.
 			RaycastHit hit;
@@ -26,6 +26,9 @@ public class ZombieAttack : MonoBehaviour
 				{
 					GameObject enemyhit = hit.transform.gameObject;
 					enemyhit.GetComponent<EnemyController>().Health--;
+					if(enemyhit.GetComponent<EnemyController>().Health <= 0){
+						Destroy(enemyhit);
+					}
 				}
 			}
 				
