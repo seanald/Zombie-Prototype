faecc291d417ce88098df1c5dc6e72fcd6ce4a08
wtshuman <wtshuman@gmail.com>
2015-10-28 15:28:04 -0400

    Better Possession

--- Test3/Assets/Scripts/Environment/FearObject.cs
+++ Test3/Assets/Scripts/Environment/FearObject.cs
@@ -2,22 +2,37 @@
 using System.Collections;
 
 public class FearObject : MonoBehaviour {
-
+	private bool isActive;
+	public GameObject ghost;
 	// Use this for initialization
 	void Start () {
-	
+		isActive = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 200f);
-		int i = 0;
-		while (i<hitColliders.Length){
-			if(hitColliders[i].tag == "EnemyScared")
-			{
-				hitColliders[i].SendMessage("IsFeared");
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			if(ghost.transform.position.magnitude - this.transform.position.magnitude < 20){
+				isActive = true;
 			}
-			i++;
 		}
+		if (isActive==true)
+		{
+			Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 200f);
+			int i = 0;
+			while(i<hitColliders.Length)
+			{
+				if (hitColliders[i].tag == "EnemyScared")
+				{
+					hitColliders[i].SendMessage("IsFeared");
+				}
+				i++;
+			}
+		}
+		print(isActive);
 	}
+
+
+
 }
