2d8bc7ea7a0322bf97710c9ac0cce9a594824e23
Chris <argon05@knights.ucf.edu>
2015-11-22 23:14:17 -0500

    Updated cemetery and mall level to contain spawners and hazards in the proper location. Fixed vampire controller facing problem. Still need to update school, town, and forest.

---
+++ Test3/Assets/Scripts/Enemy/VampireRanged/VampireRanged.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VampireRanged : Enemy {
+    public GameObject can;
+
+    public Transform throwPoint;
+    private float curLocalScale;
+
+    void Start()
+    {
+        base.Start();
+        this.curLocalScale = this.transform.localScale.x;
+    }
+
+    void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        if (this.transform.localScale.x != curLocalScale)
+        {
+            throwPoint.RotateAround(throwPoint.position, throwPoint.up, 180f);
+            this.curLocalScale = this.transform.localScale.x;
+        }
+    }
+
+    override protected void Attack()
+    {
+        if (this.distance < this.attackDistance)
+        {
+            this.enemyAnimator.Play("Vampire_Melee_Attack");
+            Instantiate(can, throwPoint.position, throwPoint.localRotation);
+            this.state = CharacterState.Moving;
+            StartCoroutine(WaitForAnimation());
+        }
+        else
+        {
+            this.Seek(distVec);
+            this.enemyAnimator.Play("Vampire_Walking");
+        }
+    }
+
+
+    override protected void Move()
+    {
+        if (this.distance >= this.dangerDistance + 50)
+        {
+            this.Seek(this.distVec);
+        }
+
+        if (this.distance < this.dangerDistance - 50)
+        {
+            this.Seek(this.distVec * -1);
+        }
+
+        this.enemyAnimator.Play("Vampire_Walking");
+
+        if (!this.strafing)
+        {
+            StartCoroutine(WaitForAttack());
+        }
+    }
+
+    override protected void Stunned()
+    {
+        this.enemyAnimator.Play("Vampire_Stunned");
+    }
+
+    IEnumerator WaitForAttack()
+    {
+        this.strafing = true;
+        yield return new WaitForSeconds(this.attackRate);
+        if (!this.checkForOtherAttackers())
+        {
+            this.state = CharacterState.Attacking;
+        }
+        this.strafing = false;
+    }
+
+    IEnumerator WaitForAnimation()
+    {
+        yield return new WaitForSeconds(1f);
+        this.Move();
+    }
+}
